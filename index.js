5f2cc2f29617a936042223841137fd3ffe7cd254
AAboran <ahmezade.anar@gmail.com>
2026-08-16 13:45:04 +0200

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-// Vercel serverless entry point. Vercel invokes this per request; we hand the
-// request straight to the Express app defined in ../server.js.
-module.exports = require('../server.js');
